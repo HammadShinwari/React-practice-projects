42723c0f6950616f48edd261810f593762210859
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-08-02 22:05:18 +0500

    react-mini-projects

---
+++ Class-Project/src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>React Developer</h1>
+      <Card />
+    </>
+  )
+}
+
+export default App
---
+++ Class-Project/src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Card() {
+    const products = [
+        {
+            name: 'ferrari',
+            price: 1000000,
+            id: 123,
+        },
+        {
+            name: 'honda',
+            price: 100000,
+            id: 456,
+        },
+        {
+            name: 'kia',
+            price: 10000,
+            id: 789,
+        }
+    ]
+  return (
+    <>
+        <h1>Card</h1>
+    </>
+  )
+}
+
+export default Card;
